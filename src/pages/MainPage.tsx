4eb0c3ba5b8e530a2ac987be2fa9402238384d72
YoavShilon05 <yoav.shilon05@gmail.com>
2026-08-30 20:58:53 +0300

    wip

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,11 +4,11 @@ import FAQSection from "@/pages/components/FaqSection/FaqSection.tsx";
 import Navbar from "@/components/Navbar/Navbar.tsx";
 import Hero from "@/pages/components/Hero/Hero.tsx";
 import TrailerSection from "@/pages/components/TrailerSection/TrailerSection.tsx";
-import InviteCTA from "@/pages/components/InviteCTA/InviteCTA.tsx";
 import VideoGallery from "@/pages/components/VideoGallery/VideoGallery.tsx";
 import AboutUs from "@/pages/components/AboutUs/AboutUs.tsx";
 import ArticlesSection from "@/pages/components/ArticlesSection/ArticlesSection.tsx";
 import ShowSchedule from "@/pages/components/ShowSchedule/ShowSchedule.tsx";
+import {AudienceComments} from "@/pages/components/AudienceComments/AudienceComments.tsx";
 
 export const MainPage = () => {
   return (
@@ -17,7 +17,8 @@ export const MainPage = () => {
         <Navbar />
         <Hero />
         <TrailerSection />
-        <InviteCTA />
+        {/*<InviteCTA />*/}
+        <AudienceComments />
         <VideoGallery />
         <AboutUs />
         <ArticlesSection />
--- src/pages/content.ts
+++ src/pages/content.ts
@@ -10,19 +10,21 @@ export const SOCIAL_LINKS = {
   facebook: "https://www.facebook.com/profile.php?id=100086619635903",
   instagram: "https://www.instagram.com/morat_ruah/",
   podcast: "https://open.spotify.com/show/4o78awCe0On3zQ3hihZhgF",
+  tiktok: "https://www.tiktok.com/@moratruah"
 }
 
 export const SCHEDULE_LINK = "https://comy.co.il/event/morat/"
 
 export const NAV_LINKS = [
-  { label: "טריילר", href: "#trailer" },
-  { label: "וידאו", href: "#videos" },
+  { label: "טעימה מהמופע", href: "#trailer" },
+  { label: "תגובות הצופים", href: "#comments" }, //todo: add comments section
   { label: "מי אנחנו", href: "#about" },
-  { label: "כתבות", href: "#press" },
+  { label: "הקליפים שלנו", href: "#videos" },
+  { label: "הפודקאסט \"שעת שהייה\"", href: SOCIAL_LINKS.podcast },
+  { label: "\"מורת רוח\" בתקשורת", href: "#press" },
   { label: "לוח הופעות", href: "#schedule" },
   { label: "שאלות ותשובות", href: "#faq" },
   { label: "צרו קשר", href: "#contact" },
-  { label: "פודקאסט", href: SOCIAL_LINKS.podcast },
 ]
 
 export const ABOUT_TEXT = `שלישיית 'מורת רוח' נולדה בשנת 2017 בחדר המורים של תיכון 'כצנלסון' בכפר סבא. לכבוד אירוע סוף שנה חגיגי במיוחד הוועד ביקש תוכנית אומנותית, וחברנו יחד.
